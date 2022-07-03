3f56773d0e4f74fdf96e2b71d81e5e7ad5dd26a9
SardorbekJavaDev <sardorbekyorqulov1@gmail.com>
2022-07-03 11:05:30 +0500

    I finished All tasks !

--- src/Task_1.java
+++ src/Task_1.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Task_1 {
+public class Task_1 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0}
+        };
+
+        int[] location = getRobotLocation(matrix);
+        if (location == null) {
+            System.out.println("Robot location not found !");
+            return;
+        }
+        System.out.println("Total steps count: " + calculateSteps(matrix, location[0], location[1]));
+    }
+
+
+    public static int[] getRobotLocation(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 1) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public static int calculateSteps(int[][] matrix, int x, int y) {
+        int center = matrix.length / 2;
+        int a = Math.abs(center - x);
+        int b = Math.abs(center - y);
+        return a + b;
+    }
+
 }
--- src/Task_2.java
+++ src/Task_2.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Task_2 {
+import java.util.Scanner;
+
+public class Task_2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("N(1≤N≤1018) [1,N]");
+        System.out.print("Enter N: ");
+        int n = scanner.nextInt();
+
+        int result = getIncreasePrimeNumbers(n);
+
+        System.out.println("Result : " + getZerosCount(result));
+
+    }
+
+    private static int getZerosCount(int result) {
+        int count = 0;
+        while (result > 0) {
+            if (result % 10 == 0) {
+                count++;
+            }
+            result /= 10;
+        }
+        return count;
+    }
+
+    private static int getIncreasePrimeNumbers(int n) {
+        boolean isPrime = true;
+        int bank = 1;
+
+        for (int i = 1; i <= n; i++) {
+            isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    System.out.println(i);
+                }
+            }
+            if (isPrime) bank *= i;
+
+        }
+        System.out.println(bank);
+        return bank;
+    }
 }
